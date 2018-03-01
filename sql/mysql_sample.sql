048a0701b01bf32989e4902ff1747e71b7d918c8
tomo0111 <tohito0430@gmail.com>
2018-03-01 17:32:38 +0900

    fix sql query

--- sql/mysql_sample.sql
+++ sql/mysql_sample.sql
@@ -4,23 +4,23 @@ DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS infos;
 
 -- users table
-CREATE TABLE `users` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `username` varchar(128) NOT NULL,
-  `email` varchar(128) NOT NULL,
-  `password` varchar(128) NOT NULL,
-  `created_at` datetime(6),
-  PRIMARY KEY (`id`),
-  UNIQUE (`email`)
+CREATE TABLE users (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  username varchar(128) NOT NULL,
+  email varchar(128) NOT NULL,
+  password varchar(128) NOT NULL,
+  created_at datetime(6),
+  PRIMARY KEY (id),
+  UNIQUE (email)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- infos table
-CREATE TABLE `infos` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(128) NOT NULL,
-  `description` varchar(128) NOT NULL,
-  `created_at` datetime(6),
-  PRIMARY KEY (`id`)
+CREATE TABLE infos (
+  id int(11) NOT NULL AUTO_INCREMENT,
+  title varchar(128) NOT NULL,
+  description varchar(128) NOT NULL,
+  created_at datetime(6),
+  PRIMARY KEY (id)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 
