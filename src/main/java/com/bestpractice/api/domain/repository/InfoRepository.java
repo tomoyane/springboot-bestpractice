d5950cf7e50d71120bc2d55d13d7330a11aef309
tomo0111 <tohito0430@gmail.com>
2018-03-01 17:33:20 +0900

    fix repository method

--- src/main/java/com/bestpractice/api/domain/repository/InfoRepository.java
+++ src/main/java/com/bestpractice/api/domain/repository/InfoRepository.java
@@ -1,12 +1,12 @@
 package com.bestpractice.api.domain.repository;
 
-import com.bestpractice.api.domain.entity.Infos;
+import com.bestpractice.api.domain.entity.InfoEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface InfoRepository extends JpaRepository<Infos, Long> {
-    List<Infos> findAll();
+public interface InfoRepository extends JpaRepository<InfoEntity, Long> {
+    List<InfoEntity> findAll();
 }
--- src/main/java/com/bestpractice/api/domain/repository/UserRepository.java
+++ src/main/java/com/bestpractice/api/domain/repository/UserRepository.java
@@ -1,10 +1,12 @@
 package com.bestpractice.api.domain.repository;
 
-import com.bestpractice.api.domain.entity.Users;
+import com.bestpractice.api.domain.entity.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository extends JpaRepository<Users, Long> {
-    Users findById(Long id);
+public interface UserRepository extends JpaRepository<UserEntity, Long> {
+    UserEntity findById(Long id);
+
+    UserEntity save(UserEntity userEntity);
 }
