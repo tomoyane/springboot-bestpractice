d2d80fed96c024719e66a48f1d137452de84bb49
tomo0111 <tohito0430@gmail.com>
2018-03-11 13:23:40 +0900

    security definition

--- src/main/java/com/bestpractice/api/common/config/SecurityConfig.java
+++ src/main/java/com/bestpractice/api/common/config/SecurityConfig.java
@@ -1,4 +1,63 @@
 package com.bestpractice.api.common.config;
 
-public class SecurityConfig {
+import com.bestpractice.api.domain.filter.PreAuthenticatedProcessingFilter;
+import com.bestpractice.api.service.AuthenticationService;
+import org.springframework.boot.autoconfigure.security.Http401AuthenticationEntryPoint;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AccountStatusUserDetailsChecker;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.AuthenticationUserDetailsService;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationProvider;
+import org.springframework.security.web.authentication.preauth.PreAuthenticatedAuthenticationToken;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public AuthenticationUserDetailsService<PreAuthenticatedAuthenticationToken> authenticationUserDetailsService() {
+        return new AuthenticationService();
+    }
+
+    @Bean
+    public PreAuthenticatedAuthenticationProvider preAuthenticatedAuthenticationProvider() {
+        System.out.print("=====provider====");
+        PreAuthenticatedAuthenticationProvider provider = new PreAuthenticatedAuthenticationProvider();
+        provider.setPreAuthenticatedUserDetailsService(authenticationUserDetailsService());
+        provider.setUserDetailsChecker(new AccountStatusUserDetailsChecker());
+        return provider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        System.out.print("=====asasasa====");
+        auth.authenticationProvider(preAuthenticatedAuthenticationProvider());
+    }
+
+    @Bean
+    public AbstractPreAuthenticatedProcessingFilter preAuthenticatedProcessingFilter() throws Exception {
+        System.out.print("=====sa====");
+        PreAuthenticatedProcessingFilter filter = new PreAuthenticatedProcessingFilter();
+        filter.setAuthenticationManager(authenticationManager());
+        return filter;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/swagger-ui.html", "/api/v1/auth")
+                .permitAll()
+                .anyRequest()
+                .authenticated()
+                .and()
+                .addFilter(preAuthenticatedProcessingFilter())
+                .exceptionHandling()
+                .authenticationEntryPoint(new Http401AuthenticationEntryPoint("headerValue"));
+    }
 }
