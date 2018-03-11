f21cede9464c8a0b136082045158e193c946a934
tomo0111 <tohito0430@gmail.com>
2018-03-11 13:24:55 +0900

    add auth filter

--- src/main/java/com/bestpractice/api/domain/filter/PreAuthenticatedProcessingFilter.java
+++ src/main/java/com/bestpractice/api/domain/filter/PreAuthenticatedProcessingFilter.java
@@ -1,4 +1,4 @@
-package com.bestpractice.api.common.filter;
+package com.bestpractice.api.domain.filter;
 
 import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
 import javax.servlet.http.HttpServletRequest;
@@ -7,11 +7,13 @@ public class PreAuthenticatedProcessingFilter extends AbstractPreAuthenticatedPr
 
     @Override
     protected Object getPreAuthenticatedPrincipal(HttpServletRequest httpServletRequest) {
+        System.out.print("=====filter1====");
         return "";
     }
 
     @Override
     protected Object getPreAuthenticatedCredentials(HttpServletRequest httpServletRequest) {
+        System.out.print("=====filter2====");
         String credentials = httpServletRequest.getHeader("Authorization: Bearer");
         return credentials == null ? "" : credentials;
     }
