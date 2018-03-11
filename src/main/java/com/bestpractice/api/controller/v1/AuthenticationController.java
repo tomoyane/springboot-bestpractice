13fd8ad99ee9610b555cacc43b943a3887bc9e0c
tomo0111 <tohito0430@gmail.com>
2018-03-11 13:24:01 +0900

    add authentication

--- src/main/java/com/bestpractice/api/controller/v1/AuthenticationController.java
+++ src/main/java/com/bestpractice/api/controller/v1/AuthenticationController.java
@@ -1,9 +1,52 @@
 package com.bestpractice.api.controller.v1;
 
+import com.bestpractice.api.domain.entity.UserEntity;
+import com.bestpractice.api.domain.entity.UserKeyEntity;
+import com.bestpractice.api.exception.Exception400;
+import com.bestpractice.api.exception.Exception409;
+import com.bestpractice.api.service.JsonWebTokenService;
+import com.bestpractice.api.service.UserService;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
-@RequestMapping("/api/v1/")
+@RequestMapping("/api/v1/auth/")
 public class AuthenticationController {
+
+    private final UserService userService;
+    private final JsonWebTokenService jwtService;
+
+    public AuthenticationController(UserService userService, JsonWebTokenService jwtService) {
+        this.userService = userService;
+        this.jwtService = jwtService;
+    }
+
+    @PostMapping
+    public Map<String, UserKeyEntity> postUser(@Validated @RequestBody UserEntity userEntity, BindingResult result) {
+
+        if (result.hasErrors()) {
+            throw new Exception400();
+        }
+
+        if (!userService.checkUserByEmail(userEntity.getEmail())) {
+            throw new Exception409();
+        }
+
+        Long userId = userService.generateUser(userEntity).getId();
+        String token = jwtService.generateJwt(userId, 100);
+        String refreshToken = jwtService.generateJwt(userId, 200);
+
+        UserKeyEntity userKeyEntity = userService.generateUserKey(userId, token, refreshToken);
+        Map<String, UserKeyEntity> map = new HashMap<>();
+        map.put("key", userKeyEntity);
+
+        return map;
+    }
 }
