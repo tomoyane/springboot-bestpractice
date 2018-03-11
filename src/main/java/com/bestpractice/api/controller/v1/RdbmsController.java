589c34ba9b95c42e73638e9ec5f5d3db1656756b
tomo0111 <tohito0430@gmail.com>
2018-03-11 13:24:17 +0900

    fix

--- src/main/java/com/bestpractice/api/controller/v1/RdbmsController.java
+++ src/main/java/com/bestpractice/api/controller/v1/RdbmsController.java
@@ -1,7 +1,7 @@
 package com.bestpractice.api.controller.v1;
 
-import com.bestpractice.api.domain.entity.UserEntity;
-import com.bestpractice.api.service.RdbmsService;
+import com.bestpractice.api.domain.entity.InfoEntity;
+import com.bestpractice.api.service.InfoService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
@@ -9,50 +9,50 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/v1/users/")
+@RequestMapping("/api/v1/infos/")
 public class RdbmsController {
 
-    private final RdbmsService rdbmsService;
+    private final InfoService infoService;
 
-    public RdbmsController(RdbmsService rdbmsService) {
-        this.rdbmsService = rdbmsService;
+    public RdbmsController(InfoService infoService) {
+        this.infoService = infoService;
     }
 
     @GetMapping()
-    public Map<String, List<UserEntity>> getUserList() {
-        Map<String, List<UserEntity>> map = new HashMap<>();
-        map.put("users", rdbmsService.getUserList());
+    public Map<String, List<InfoEntity>> getInfos() {
+        Map<String, List<InfoEntity>> map = new HashMap<>();
+        map.put("infos", infoService.getInfoList());
         return map;
     }
 
     @GetMapping(value="{id}")
-    public Map<String, UserEntity> getUser(@PathVariable("id") Long id) {
-        Map<String, UserEntity> map = new HashMap<>();
-        map.put("user", rdbmsService.getUser(id));
+    public Map<String, InfoEntity> getInfo(@PathVariable("id") Long id) {
+        Map<String, InfoEntity> map = new HashMap<>();
+        map.put("info", infoService.getInfo(id));
         return map;
     }
 
     @PostMapping
-    public Map<String, UserEntity> postUser(@RequestBody UserEntity userEntity) {
-        rdbmsService.generateUser(userEntity);
-        Map<String, UserEntity> map = new HashMap<>();
-        map.put("user", userEntity);
+    public Map<String, InfoEntity> postInfo(@RequestBody InfoEntity infoEntity) {
+        infoService.generateInfo(infoEntity);
+        Map<String, InfoEntity> map = new HashMap<>();
+        map.put("info", infoEntity);
         return map;
     }
 
     @PutMapping
-    public Map<String, UserEntity> putUser(@RequestBody UserEntity userEntity) {
-        rdbmsService.generateUser(userEntity);
-        Map<String, UserEntity> map = new HashMap<>();
-        map.put("user", userEntity);
+    public Map<String, InfoEntity> putInfo(@RequestBody InfoEntity infoEntity) {
+        infoService.generateInfo(infoEntity);
+        Map<String, InfoEntity> map = new HashMap<>();
+        map.put("info", infoEntity);
         return map;
     }
 
     @DeleteMapping(value = "{id}")
-    public Map<String, String> deleteUser(@PathVariable("id") Long id) {
-        rdbmsService.deleteUser(id);
+    public Map<String, String> deleteInfo(@PathVariable("id") Long id) {
+        infoService.deleteInfo(id);
         Map<String, String> map = new HashMap<>();
-        map.put("user", "success.");
+        map.put("info", "success.");
         return map;
     }
 }
