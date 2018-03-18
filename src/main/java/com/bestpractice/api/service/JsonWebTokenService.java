10a6aaea03f4aa3140e004319e3928808dd3e14f
tomo0111 <tohito0430@gmail.com>
2018-03-18 11:58:50 +0900

    add generate token, decode process

--- src/main/java/com/bestpractice/api/service/JsonWebTokenService.java
+++ src/main/java/com/bestpractice/api/service/JsonWebTokenService.java
@@ -1,21 +1,24 @@
 package com.bestpractice.api.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
+import java.util.Base64;
 import java.util.Date;
 
 @Service
 public class JsonWebTokenService {
 
     public String generateJwt(Long userId, int period) {
-
         Key key = MacProvider.generateKey();
         LocalDateTime now = LocalDateTime.now();
         ZonedDateTime expirationDate = now.plusDays(period).atZone(ZoneId.systemDefault());
@@ -25,5 +28,20 @@ public class JsonWebTokenService {
                 .setExpiration(Date.from(expirationDate.toInstant()))
                 .signWith(SignatureAlgorithm.HS512, key)
                 .compact();
+    }
+
+    public Long decodeJwt(String jwt) {
+        String[] jwtSections = jwt.split("\\.");
+        String claim = new String(Base64.getDecoder().decode(jwtSections[1]));
+        ObjectMapper mapper = new ObjectMapper();
+
+        try {
+            JsonNode obj = mapper.readTree(claim);
+            return Long.parseLong(obj.get("sub").toString());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return null;
     }
 }
