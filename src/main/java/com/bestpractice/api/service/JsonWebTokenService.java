4d9bb6b90528abdecec6bba7bb00bdcdc7f1bf6c
tomo0111 <tohito0430@gmail.com>
2018-03-18 23:28:20 +0900

    fix logic

--- src/main/java/com/bestpractice/api/service/JsonWebTokenService.java
+++ src/main/java/com/bestpractice/api/service/JsonWebTokenService.java
@@ -1,13 +1,13 @@
 package com.bestpractice.api.service;
 
-import com.fasterxml.jackson.databind.JsonNode;
+import com.bestpractice.api.common.property.AppProperty;
+import com.bestpractice.api.domain.model.CredentialModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
 import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -18,30 +18,40 @@ import java.util.Date;
 @Service
 public class JsonWebTokenService {
 
-    public String generateJwt(Long userId, int period) {
-        Key key = MacProvider.generateKey();
+    private final AppProperty appProperty;
+
+    public JsonWebTokenService(AppProperty appProperty) {
+        this.appProperty = appProperty;
+    }
+
+    public Key generateSignature() {
+        return MacProvider.generateKey();
+    }
+
+    public String generateJwt(Key key, Long userId, int period, String uuid) {
         LocalDateTime now = LocalDateTime.now();
         ZonedDateTime expirationDate = now.plusDays(period).atZone(ZoneId.systemDefault());
 
         return Jwts.builder()
+                .setIssuer(appProperty.getProvider())
                 .setSubject(String.valueOf(userId))
+                .setAudience(appProperty.getSubject())
+                .setId(uuid)
                 .setExpiration(Date.from(expirationDate.toInstant()))
                 .signWith(SignatureAlgorithm.HS512, key)
                 .compact();
     }
 
-    public Long decodeJwt(String jwt) {
-        String[] jwtSections = jwt.split("\\.");
-        String claim = new String(Base64.getDecoder().decode(jwtSections[1]));
-        ObjectMapper mapper = new ObjectMapper();
-
+    public CredentialModel decodeJwt(String jwt) {
         try {
-            JsonNode obj = mapper.readTree(claim);
-            return Long.parseLong(obj.get("sub").toString());
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+            String[] jwtSections = jwt.split("\\.");
+            String claim = new String(Base64.getDecoder().decode(jwtSections[1]));
+            ObjectMapper mapper = new ObjectMapper();
 
-        return null;
+            return mapper.<CredentialModel>readValue(claim, CredentialModel.class);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new CredentialModel();
+        }
     }
 }
