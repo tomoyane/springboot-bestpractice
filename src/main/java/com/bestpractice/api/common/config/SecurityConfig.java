1353698656bd6df77be4bd136ae81698f6abb66d
tomo0111 <tohito0430@gmail.com>
2018-03-18 23:26:48 +0900

    fix security http

--- src/main/java/com/bestpractice/api/common/config/SecurityConfig.java
+++ src/main/java/com/bestpractice/api/common/config/SecurityConfig.java
@@ -59,15 +59,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    public void configure(WebSecurity web) throws Exception {
-        web.ignoring()
-                .antMatchers(
-                        "/v2/api-docs",
-                        "/configuration/ui",
-                        "/swagger-resources",
-                        "/configuration/security",
-                        "/swagger-ui.html",
-                        "/webjars/**"
-                );
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers(
+                "/v2/api-docs",
+                "/configuration/ui",
+                "/swagger-resources",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**"
+        );
     }
 }
