e35e3d329809dffc1c7fa9dabd535352e6e0158d
tomo0111 <tohito0430@gmail.com>
2018-03-18 23:27:02 +0900

    fix

--- src/main/java/com/bestpractice/api/common/config/SwaggerConfig.java
+++ src/main/java/com/bestpractice/api/common/config/SwaggerConfig.java
@@ -3,11 +3,15 @@ package com.bestpractice.api.common.config;
 import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Parameter;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 
 @Configuration
@@ -16,6 +20,7 @@ public class SwaggerConfig {
 
     @Bean
     public Docket swaggerSpringMvcPlugin() {
+
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .paths(Predicates.not(PathSelectors.regex("/error")))
