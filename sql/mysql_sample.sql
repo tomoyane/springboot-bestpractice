aba27a4e739804e0cb3bd8a1031e1a13546e1749
tomo0111 <tohito0430@gmail.com>
2018-03-21 16:35:54 +0900

    ufix sql

--- sql/mysql_sample.sql
+++ sql/mysql_sample.sql
@@ -20,9 +20,11 @@ CREATE TABLE users (
 -- user_keys table
 CREATE TABLE user_keys (
   id int(11) NOT NULL AUTO_INCREMENT,
+  token_type varchar(128) NOT NULL,
   token varchar(256) NOT NULL,
   refresh_token varchar(256) NOT NULL,
   user_id int(11) NOT NULL,
+  expires_at datetime(6),
   created_at datetime(6),
   PRIMARY KEY (id),
   UNIQUE (token),
