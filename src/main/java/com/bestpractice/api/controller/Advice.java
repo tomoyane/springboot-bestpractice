dc1ec6d789f31fb60446ac0f0e52f3c437ace32e
tomo0111 <tohito0430@gmail.com>
2018-03-21 22:44:17 +0900

    add conflict exception

--- src/main/java/com/bestpractice/api/controller/Advice.java
+++ src/main/java/com/bestpractice/api/controller/Advice.java
@@ -4,6 +4,7 @@ import com.bestpractice.api.domain.model.ExceptionModel;
 import com.bestpractice.api.exception.Exception400;
 import com.bestpractice.api.exception.Exception401;
 import com.bestpractice.api.exception.Exception403;
+import com.bestpractice.api.exception.Exception409;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -12,11 +13,11 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
+@ResponseBody
 public class Advice {
 
     @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(Exception400.class)
-    @ResponseBody
     public ExceptionModel badRequest() {
         ExceptionModel exceptionModel = new ExceptionModel();
         exceptionModel.setStatus(400);
@@ -27,7 +28,6 @@ public class Advice {
 
     @ResponseStatus(HttpStatus.UNAUTHORIZED)
     @ExceptionHandler(Exception401.class)
-    @ResponseBody
     public ExceptionModel unAuthorized() {
         ExceptionModel exceptionModel = new ExceptionModel();
         exceptionModel.setStatus(401);
@@ -38,7 +38,6 @@ public class Advice {
 
     @ResponseStatus(HttpStatus.FORBIDDEN)
     @ExceptionHandler(Exception403.class)
-    @ResponseBody
     public ExceptionModel forbidden() {
         ExceptionModel exceptionModel = new ExceptionModel();
         exceptionModel.setStatus(403);
@@ -49,12 +48,21 @@ public class Advice {
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
-    @ResponseBody
     public ExceptionModel notFound() {
         ExceptionModel exceptionModel = new ExceptionModel();
         exceptionModel.setStatus(404);
         exceptionModel.setError("Not found");
-        exceptionModel.setMessage("");
+        exceptionModel.setMessage("Not found path");
+        return exceptionModel;
+    }
+
+    @ResponseStatus(HttpStatus.CONFLICT)
+    @ExceptionHandler(Exception409.class)
+    public ExceptionModel conflict() {
+        ExceptionModel exceptionModel = new ExceptionModel();
+        exceptionModel.setStatus(409);
+        exceptionModel.setError("Conflict");
+        exceptionModel.setMessage("Already exist email");
         return exceptionModel;
     }
 }
