dccf3617da8127fae3dc406f087e17fe223e6c4a
tomo0111 <tohito0430@gmail.com>
2018-03-21 16:36:28 +0900

    add method

--- src/main/java/com/bestpractice/api/common/util/Util.java
+++ src/main/java/com/bestpractice/api/common/util/Util.java
@@ -1,4 +1,17 @@
 package com.bestpractice.api.common.util;
 
+import java.util.Calendar;
+import java.util.Date;
+
 public class Util {
+
+    public static Date calculateDate() {
+        Date date = new Date();
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(date);
+        calendar.add(Calendar.YEAR, 1);
+        date = calendar.getTime();
+
+        return date;
+    }
 }
