691ed8abccf5f71705d378d8780342117305e03d
tomo0111 <tohito0430@gmail.com>
2018-03-22 23:55:34 +0900

    add property

--- src/main/java/com/bestpractice/api/common/property/CredentialProperty.java
+++ src/main/java/com/bestpractice/api/common/property/CredentialProperty.java
@@ -4,17 +4,43 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
-@ConfigurationProperties(prefix = "settings")
-public class AppProperty {
+@ConfigurationProperties(prefix = "credentials")
+public class CredentialProperty {
 
+    private String key;
     private String provider;
     private String subject;
+    private String alg;
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
 
     public String getProvider() {
         return provider;
     }
 
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
     public String getSubject() {
         return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAlg() {
+        return alg;
+    }
+
+    public void setAlg(String alg) {
+        this.alg = alg;
     }
 }
