7946a6b5f3e9f54c70aa3ce2655735adabaeaecf
tomo0111 <tohito0430@gmail.com>
2018-03-24 18:59:10 +0900

    add secret vertification

--- src/main/java/com/bestpractice/api/service/JsonWebTokenService.java
+++ src/main/java/com/bestpractice/api/service/JsonWebTokenService.java
@@ -1,14 +1,12 @@
 package com.bestpractice.api.service;
 
-import com.bestpractice.api.common.property.AppProperty;
+import com.bestpractice.api.common.property.CredentialProperty;
 import com.bestpractice.api.domain.model.CredentialModel;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
-import io.jsonwebtoken.impl.crypto.MacProvider;
 import org.springframework.stereotype.Service;
 
-import java.security.Key;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.ZonedDateTime;
@@ -18,40 +16,45 @@ import java.util.Date;
 @Service
 public class JsonWebTokenService {
 
-    private final AppProperty appProperty;
+    private final CredentialProperty credentialProperty;
 
-    public JsonWebTokenService(AppProperty appProperty) {
-        this.appProperty = appProperty;
+    public JsonWebTokenService(CredentialProperty credentialProperty) {
+        this.credentialProperty = credentialProperty;
     }
 
-    public Key generateSignature() {
-        return MacProvider.generateKey();
-    }
-
-    public String generateJwt(Key key, Long userId, int period, String uuid) {
+    public String generateJwt(Long userId, int period, String uuid) {
         LocalDateTime now = LocalDateTime.now();
         ZonedDateTime expirationDate = now.plusDays(period).atZone(ZoneId.systemDefault());
 
         return Jwts.builder()
-                .setIssuer(appProperty.getProvider())
+                .setIssuer(credentialProperty.getProvider())
                 .setSubject(String.valueOf(userId))
-                .setAudience(appProperty.getSubject())
+                .setAudience(credentialProperty.getSubject())
                 .setId(uuid)
                 .setExpiration(Date.from(expirationDate.toInstant()))
-                .signWith(SignatureAlgorithm.HS512, key)
+                .signWith(SignatureAlgorithm.HS512, credentialProperty.getKey().getBytes())
                 .compact();
     }
 
     public CredentialModel decodeJwt(String jwt) {
         try {
-            String[] jwtSections = jwt.split("\\.");
-            String claim = new String(Base64.getDecoder().decode(jwtSections[1]));
             ObjectMapper mapper = new ObjectMapper();
 
-            return mapper.<CredentialModel>readValue(claim, CredentialModel.class);
+            String[] jwtSections = jwt.split("\\.");
+            String header = new String(Base64.getDecoder().decode(jwtSections[0]));
+
+            if (mapper.readTree(header).get("alg").asText().equals(credentialProperty.getAlg())) {
+                String claim = new String(Base64.getDecoder().decode(jwtSections[1]));
+
+                Jwts.parser().setSigningKey(credentialProperty.getKey().getBytes()).parseClaimsJws(jwt);
+
+                return mapper.<CredentialModel>readValue(claim, CredentialModel.class);
+            }
+
         } catch (Exception e) {
             e.printStackTrace();
-            return new CredentialModel();
         }
+
+        return new CredentialModel();
     }
 }
