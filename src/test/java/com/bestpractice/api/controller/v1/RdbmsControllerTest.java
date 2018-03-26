029737b5397bde6a29369bd3065d37813b904ffd
tomo0111 <tohito0430@gmail.com>
2018-03-26 22:03:34 +0900

    add rdmscontroller test

--- src/test/java/com/bestpractice/api/controller/v1/RdbmsControllerTest.java
+++ src/test/java/com/bestpractice/api/controller/v1/RdbmsControllerTest.java
@@ -1,5 +1,28 @@
+package com.bestpractice.api.controller.v1;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class RdbmsControllerTest {
 
+    @Test
+    public void getInfos() {
+    }
+
+    @Test
+    public void getInfo() {
+    }
+
+    @Test
+    public void postInfo() {
+    }
+
+    @Test
+    public void putInfo() {
+    }
+
+    @Test
+    public void deleteInfo() {
+    }
 }
