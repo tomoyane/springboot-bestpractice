7ed96795fe12bd88b42eb39e61b97a2c72a7d51c
tomo0111 <tohito0430@gmail.com>
2018-03-26 22:03:07 +0900

    add mysql docker container

--- src/test/java/com/bestpractice/api/MySqlContainerTest.java
+++ src/test/java/com/bestpractice/api/MySqlContainerTest.java
@@ -1,4 +1,15 @@
 package com.bestpractice.api;
 
-public class MySqlContainerUsingTest {
+import org.junit.FixMethodOrder;
+import org.junit.runners.MethodSorters;
+import org.testcontainers.containers.MySQLContainer;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class MySqlContainerTest {
+
+    private static MySQLContainer MYSQL = new MySQLContainer("mysql");
+
+    static MySQLContainer database() {
+        return MYSQL;
+    }
 }
