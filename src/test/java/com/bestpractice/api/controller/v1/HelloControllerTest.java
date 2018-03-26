390305864722a8049ed8949d5d2d4dd81fdd0772
tomo0111 <tohito0430@gmail.com>
2018-03-26 22:03:22 +0900

    add hellocontroller test

--- src/test/java/com/bestpractice/api/controller/v1/HelloControllerTest.java
+++ src/test/java/com/bestpractice/api/controller/v1/HelloControllerTest.java
@@ -1,15 +1,18 @@
-package com.bestpractice.api.controller;
+package com.bestpractice.api.controller.v1;
 
-
-import com.bestpractice.api.controller.v1.HelloController;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class HelloControllerTest {
 
     private MockMvc mvc;
