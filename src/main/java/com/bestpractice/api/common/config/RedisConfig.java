2ca88f8f399a28cb3726f8685cd0b41323243510
tomo0111 <tohito0430@gmail.com>
2018-03-31 11:30:25 +0900

    add redis configulation

--- src/main/java/com/bestpractice/api/common/config/RedisConfig.java
+++ src/main/java/com/bestpractice/api/common/config/RedisConfig.java
@@ -1,4 +1,44 @@
 package com.bestpractice.api.common.config;
 
+import com.bestpractice.api.common.property.RedisProperty;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.JdkSerializationRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+@EnableCaching
 public class RedisConfig {
+
+    @Autowired
+    RedisProperty redisProperty;
+
+    @Bean
+    public JedisConnectionFactory connectionFactory() {
+        JedisConnectionFactory jedisConnectionFactory = new JedisConnectionFactory();
+        jedisConnectionFactory.setHostName(redisProperty.getHost());
+        jedisConnectionFactory.setPort(redisProperty.getPort());
+        jedisConnectionFactory.setUsePool(true);
+        return jedisConnectionFactory;
+    }
+
+    @Bean
+    public RedisTemplate redisTemplate() {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(connectionFactory());
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        redisTemplate.setValueSerializer(new JdkSerializationRedisSerializer());
+        redisTemplate.afterPropertiesSet();
+        return redisTemplate;
+    }
+
+    @Bean
+    public RedisCacheManager redisCacheManager() {
+        return new RedisCacheManager(redisTemplate());
+    }
 }
