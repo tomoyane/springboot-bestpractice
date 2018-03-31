20e39126ff165087f27ec75d169ef11f33934f78
tomo0111 <tohito0430@gmail.com>
2018-03-31 11:31:04 +0900

    add redis property

--- src/main/java/com/bestpractice/api/common/property/RedisProperty.java
+++ src/main/java/com/bestpractice/api/common/property/RedisProperty.java
@@ -1,4 +1,36 @@
 package com.bestpractice.api.common.property;
 
-public class CassandraProperty {
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "spring.redis")
+public class RedisProperty {
+    private String host;
+    private int port;
+    private String password;
+
+    public String getHost() {
+        return host;
+    }
+
+    public void setHost(String host) {
+        this.host = host;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
