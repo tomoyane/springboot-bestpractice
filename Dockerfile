8ec59d93dbe35b308ad4142cb99138b51f0c5fb1
tomo0111 <tohito0430@gmail.com>
2018-04-05 23:43:26 +0900

    fix command

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,9 @@ FROM openjdk:8-jdk-alpine
 
 VOLUME /tmp
 
+ENV appName="spring-boot-bestpractice"
+ENV version="0.0.1"
+
 RUN mkdir /spring-boot-bestpractice
 WORKDIR /spring-boot-bestpractice
 
@@ -10,3 +13,5 @@ RUN adduser -S sboot
 USER sboot
 
 COPY . /spring-boot-bestpractice
+
+CMD java -Djava.security.egd=file:/dev/./urandom -jar build/libs/${appName}-${version}-SNAPSHOT.jar
---
+++ src/main/java/com/bestpractice/api/domain/entity/TaskEntity.java
@@ -0,0 +1,49 @@
+package com.bestpractice.api.domain.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "infos")
+public class InfoEntity extends AbstractEntity {
+
+    @Id
+    @Column(name = "id")
+    @JsonIgnore
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, name = "title")
+    private String title;
+
+    @NotNull
+    @Column(nullable = false, name = "description")
+    private String description;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
