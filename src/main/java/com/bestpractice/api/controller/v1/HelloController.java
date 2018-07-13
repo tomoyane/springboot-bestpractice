83063d5e26fb7bd7c567881d5acd17924604de27
tomo0111 <tohito0430@gmail.com>
2018-07-13 13:29:04 +0900

    refactor add constructor variable 'this'

--- src/main/java/com/bestpractice/api/controller/v1/HelloController.java
+++ src/main/java/com/bestpractice/api/controller/v1/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Map;
 
 @RestController
@@ -15,8 +15,6 @@ public class HelloController {
     @ResponseBody
     @GetMapping(value="/hello")
     public Map<String, String> sample1() {
-        Map<String, String> map = new HashMap<>();
-        map.put("key", "Hello world");
-        return map;
+        return Collections.singletonMap("key", "Hello world.");
     }
 }
