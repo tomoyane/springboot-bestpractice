b760eae8d9b22def241e2efc035b8525bd31f358
tomoyane <tomoyane111@gmail.com>
2024-01-28 14:42:14 +0900

    fix build action

--- .github/scripts/build.sh
+++ .github/scripts/build.sh
@@ -2,9 +2,9 @@
 
 set -e
 
-ver=$(cat build.gradle | grep version | sed 's/version =//g' | sed "s/'//g" | sed -n 1P | sed 's/\"//g' | sed 's/ //g')
+ver=$(cat build.gradle | grep pkgVersion | sed 's/def pkgVersion =//g' | sed "s/'//g" | sed -n 1P | sed 's/\"//g' | sed 's/ //g')
 
 ./gradlew clean build -x test
 docker login -u "${DOCKER_USER}" -p "${DOCKER_PASSWORD}"
-docker build -t tomohito/springboot-bestpractice:"${ver}" -t tomohito/springboot-bestpractice:latest .
+docker build -f docker/Dockerfile.app -t tomohito/springboot-bestpractice:"${ver}" -t tomohito/springboot-bestpractice:latest .
 docker push tomohito/springboot-bestpractice
